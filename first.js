fa593907e08e6f1025973ccb68c5ee81b8ae7389
Kethan S A <74355361+Kethan-SA@users.noreply.github.com>
2021-08-04 21:13:57 +0530

    Add files via upload

--- first.js
+++ first.js
@@ -1,7 +1,21 @@
-let a=10;
-let b=10;
-console.log(c=a+b);
-console.log(c=a-b);
-console.log(c=a*b);
-console.log(c=a/b);
+// let a=10;
+// let b=10;
+// console.log(c=a+b);
+// console.log(c=a-b);
+// console.log(c=a*b);
+// console.log(c=a/b);
 
+
+let arr=["Batman","Flash","Superman","GreenArrow","WonderWomen","GreenLantern"];
+
+console.log(arr.length);
+
+for(let i=5;i>=0;i--){s
+
+    console.log(arr[i],i);
+
+}
+
+for(let i=0; i<=50; i % 5 == 0) {
+    console.log(i);
+}
