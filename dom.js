64934f24703e7af7e95676d0aec922bc4e3c7c38
Kethan S A <74355361+Kethan-SA@users.noreply.github.com>
2021-08-05 15:25:37 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,10 @@
+// document.querySelector("#sec").innerText="LetsUpgrade";
+function dosomething() {
+    let name=document.querySelector("#imp").value;
+    document.querySelector("#sec").innerHTML=`<h1>${name}</h1>`;
+    let city=document.querySelector("#imp1").value;
+    document.querySelector("#sec1").innerHTML=`<h1>${city}</h1>`;
+}
+
+
+
